2c0af964bffed5921fdacfe6ba7046b8c2243e82
likaf9 <115149370+likaf9@users.noreply.github.com>
2023-03-04 21:10:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -22,3 +22,36 @@ print(len(s.encode('cp1251')))
 print(len(s.encode('utf8')))
 print(len(s.encode('utf16')))
 print(len(s.encode('utf32')))
+
+
+a = 1a = 1
+b = 2
+f = (a - b) * (a + b)
+print(f)
+f = a**2 - 2*a*b + b**2
+print(f)
+f = a**2 + 2*a*b + b**2
+print(f)
+f= a**3 - 3*a**2*b + 3*a*b**2 - b**3
+print(f)
+f = a**3 + 3*a**2*b + 3*a*b**2 - b**3
+print(f)
+f= (a + b)*(a**2 + a*b + b**2)
+print(f)
+f = (a - b)*(a**2 + a*b + b**2)
+print(f)
+
+
+a = 1
+b = 2
+c = 3
+f = ((((a * a) + ((a + c)**2) / (10 * b))- (c**2 * a * 4))**(-1/2))
+
+
+
+number = 0.1
+while number <= 0.3:
+    number += 0.1
+    print(number)
+number = round(number, 1)
+print(number)
